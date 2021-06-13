1b7d094f9a0efe283396aee740e7cc700e08407b
gldn-god <78330405+gldn-god@users.noreply.github.com>
2021-06-12 23:14:42 -0600

    data table append finished for level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,25 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// body reference
+var tbody = d3.select("tbody");
+
+// sightings
+tableData.forEach(function(ufoSighting) {
+    
+    // write sighting to console
+    console.log(ufoSighting);
+
+    // append row for each sighting
+    var row = tbody.append("tr");
+
+    // return array and output to web console
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      
+      // append cell to row
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
